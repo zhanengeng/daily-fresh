b8e3f4066ea57b16e445064b2ad41a0a27aab73f
zhanengeng <zhanengeng@gmail.com>
2020-06-06 09:31:57 +0900

    注册功能配置完成（celery ok）

--- dailyfresh/apps/user/views.py
+++ dailyfresh/apps/user/views.py
@@ -8,73 +8,15 @@ from django.views import View # 导入类视图
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer# 加密用模块
 from itsdangerous import SignatureExpired # 加密过期时的异常
 from django.conf import settings # 导入settings.py.这里用于导入秘钥
-from django.core.mail import send_mail # 发送邮件模块
+# from django.core.mail import send_mail # 发送邮件模块
+from celery_tasks.tasks import send_register_active_email
+# 导入自定义的celery发送邮件模块
 
 import re # 正则
 
 # Create your views here.
 
 # /user/register
-def register(request):
-    '''登録画面（getリクエスト）＆登録処理（Postリクエスト）'''
-    if request.method == "GET":
-        # 若为get请求，表示登録画面
-        return render(request, 'register.html')
-
-    elif request.method == "POST":
-        # 若为post请求，说明是表单提出，进行登録処理
-        # 1.接收数据
-        username = request.POST.get("user_name")
-        password = request.POST.get("pwd")
-        email = request.POST.get("email")
-        allow = request.POST.get("allow")
-
-        # 2.进行校验
-        # 2-1.校验数据完整性
-        if not all([username, password, email]):
-            # 数据不完整
-            return render(request, 'register.html', {"errmsg":"数据不完整"})
-        
-        # 2-2.校验邮箱
-        if not re.match(r'^[a-z0-9][\w.\-]*@[a-z0-9\-]+(\.[a-z]{2,5}){1,2}$', email):
-            return render(request, "register.html", {"errmsg":"邮箱格式不正确"})
-
-        # 2-3.是否勾选同意
-        if allow != "on":
-            return render(request, "register.html", {"errmsg":"请同意协议"})
-
-        # 2-4.用户名是否重复
-        try:
-            # 用户名重复
-            user = User.objects.get(username=username)
-        except User.DoesNotExist:
-            # 用户名还未被注册
-            user = None
-
-        if user:
-            # 用户名已存在
-            return render(request, "register.html", {"errmsg":"用户名已存在"})
-
-        # 3.业务处理：进行用户注册
-        # 一般方法：
-        # user = User()
-        # user.username = username
-        # user.password = password
-        # ...
-        # user.save()
-
-        # 利用django自带注册模块：
-        user = User.objects.create_user(username, email, password) 
-        user.is_active = 0
-        user.save()
-
-        # 4.返回应答，跳转首页。这里利用了重定向和反向解析
-        return redirect(reverse('goods:index'))
-
-def register_handle(request):
-    '''和上方的post处理相同，可以用一个urls来处理，也可以这样分开来处理'''
-    pass
-
 class RegisterView(View):
     '''自己定义类视图，用于相应不同的解决方法'''
     def get(self, request):
@@ -129,15 +71,8 @@ class RegisterView(View):
         token = serializer.dumps(info) # 利用实例对象的dumps方法进行加密.格式：bytes
         token = token.decode() # 把bytes => utf8
 
-        # 3-2 发邮件
-        subject = "天天生鲜欢迎信息"
-        message = "" # 如果信息带格式（<h1>），用html_message发送
-        html_message = f"<h1>{username},欢迎您成为天天生鲜会员</h1>请点击下面链接激活账户<br/><a href='http://127.0.0.1:8000/user/active/{token}'>http://127.0.0.1:8000/user/active/{token}</a>" 
-        # 邮件地址的重点是user/active。向active发送token
-        sender = settings.DEFAULT_FROM_EMAIL
-        receiver_list = [email]
-        send_mail(subject, message, sender, receiver_list, html_message=html_message)
-
+        # 3-2 发邮件（利用celery来发送）
+        send_register_active_email.delay(email, username, token)
         # 3-3 激活(用类ActiveView激活)
         # 4.返回应答，跳转首页。这里利用了重定向和反向解析
         return redirect(reverse('goods:index'))       
---
+++ dailyfresh/celery_tasks/tasks.py
@@ -0,0 +1,34 @@
+# 使用celery
+from celery import Celery
+from django.conf import settings
+from django.core.mail import send_mail # 发送邮件模块
+
+# 为任务处理者(worker)进行django环境初始化，这段代码写于worker所在的电脑
+#================================
+import os
+import django
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dailyfresh.settings') 
+django.setup()
+#================================
+
+
+# 创建celery类的实例对象
+#================================
+app = Celery("celery_tasks.tasks", broker="redis://127.0.0.1:6379/1")
+#================================
+
+
+# 定义任务函数
+#================================
+@app.task
+def send_register_active_email(to_email, username, token):
+    '''发送激活邮件'''
+    # 阻止邮件信息
+    subject = "天天生鲜欢迎信息"
+    message = "" # 如果信息带格式（<h1>），用html_message发送
+    html_message = f"<h1>{username},欢迎您成为天天生鲜会员</h1>请点击下面链接激活账户<br/><a href='http://127.0.0.1:8000/user/active/{token}'>http://127.0.0.1:8000/user/active/{token}</a>" 
+    # 邮件地址的重点是user/active。向active发送token
+    sender = settings.DEFAULT_FROM_EMAIL
+    receiver_list = [to_email]
+    send_mail(subject, message, sender, receiver_list, html_message=html_message)
+#================================
